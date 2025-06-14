0a353b3f0d3b4b7156206fa869faecacf5ae4989
DraganM03 <draganmijatovic.s1a@gmail.com>
2025-06-14 16:13:57 +0200

    fixed errorHandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  res.status(404).json({
+  res.status(err.status).json({
     error: {
       message: err.message,
       status: err.status || 500,
