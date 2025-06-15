6c5925bac603c95c54179ea33a47a27ffef35d3c
DraganM03 <draganmijatovic.s1a@gmail.com>
2025-06-15 19:35:30 +0200

    added file upload support for register

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 import express from 'express';
-
+import multer from 'multer';
 import { loginUser, registerUser } from '../controllers/userController.js';
 
 const router = express.Router();
+const upload = multer({ dest: 'static/images' });
 
 router.post('/login', loginUser);
-router.post('/register', registerUser);
+router.post('/register', upload.single('profile_image'), registerUser);
 
 export default router;
